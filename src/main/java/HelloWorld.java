ad674f7e9cba61a72d3573f156280f469e27eb77
hclswlaunch <77541238+hclswlaunch@users.noreply.github.com>
2021-11-29 10:21:42 +0000

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World application");
+		get("/hello", (req, res) -> "Hello World applicatin");
 	}
 }
